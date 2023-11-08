8d91862c7cbb5af4478cd15597eff9f917e360c3
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-07 19:47:50 -0800

    Palindrome Complete

--- solutions.js
+++ solutions.js
@@ -3,10 +3,10 @@
 //=========================================
 
 // 1. What is the difference between a parameter and an argument?
-// The difference between Argument and Parameter is that Parameter is passed in 
+// The difference between Argument and Parameter is that Parameter is passed in
 // the the function where Arguments are passed outside.
 // 2. Within a function, what is the difference between return and console.log?
-// Return is just returning the value of the function but not printing in in the 
+// Return is just returning the value of the function but not printing in in the
 // console as a console.log would do.
 
 //=========================================
@@ -18,4 +18,23 @@
 // is not. Make sure your function will give the correct answer for words with
 // capital letters.
 
+const checkPalindrome = (str) => {
+  str = str.toLowerCase().replace(/\W/g,"");
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return `${str} is not a Palindrome`;
+    }
+  }
+  return `${str} is a Palindrome`;
+};
+console.log(checkPalindrome("RADAR"));
+console.log(checkPalindrome("mopar"));
+console.log(checkPalindrome("A MAN, a plan, a CANAL, pAnAmA"));
+console.log(checkPalindrome("RACEcar"));
+console.log(checkPalindrome("Anna"));
+console.log(checkPalindrome("GrandMa"));
+console.log(checkPalindrome("catNip"))
 
+//=========================================
+// Sum Array
+//=========================================
