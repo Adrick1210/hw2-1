43bb8b9718d3049409a5f1780e3a95e4884af234
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-07 19:03:13 -0800

    Verbal Questions Answered

---
+++ solutions.js
@@ -0,0 +1,21 @@
+//=========================================
+// Section 1
+//=========================================
+
+// 1. What is the difference between a parameter and an argument?
+// The difference between Argument and Parameter is that Parameter is passed in 
+// the the function where Arguments are passed outside.
+// 2. Within a function, what is the difference between return and console.log?
+// Return is just returning the value of the function but not printing in in the 
+// console as a console.log would do.
+
+//=========================================
+// Palindrome
+//=========================================
+
+// Write a function checkPalindrome that accepts a single argument, a string. The
+// function should return true (Boolean) if the string is a palindrome, false if it
+// is not. Make sure your function will give the correct answer for words with
+// capital letters.
+
+
