42a397a5dd236606edebf3409e377930f8802c78
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-07 22:04:18 -0800

    work in progress

--- solutions.js
+++ solutions.js
@@ -18,53 +18,53 @@
 // is not. Make sure your function will give the correct answer for words with
 // capital letters.
 
-const checkPalindrome = (str) => {
-  str = str.toLowerCase().replace(/\W/g, "");
-  for (let i = 0; i < str.length; i += 1) {
-    if (str[i] !== str[str.length - 1 - i]) {
-      return `${str} is not a Palindrome`;
-    }
-  }
-  return `${str} is a Palindrome`;
-};
-console.log(checkPalindrome("RADAR")); // true
-console.log(checkPalindrome("mopar")); // false
-console.log(checkPalindrome("A MAN, a plan, a CANAL, pAnAmA")); // true
-console.log(checkPalindrome("RACEcar")); //true
-console.log(checkPalindrome("Anna")); // true
-console.log(checkPalindrome("GrandMa")); // false
-console.log(checkPalindrome("catNip")); // false
+// const checkPalindrome = (str) => {
+//   str = str.toLowerCase().replace(/\W/g, "");
+//   for (let i = 0; i < str.length; i += 1) {
+//     if (str[i] !== str[str.length - 1 - i]) {
+//       return `${str} is not a Palindrome`;
+//     }
+//   }
+//   return `${str} is a Palindrome`;
+// };
+// console.log(checkPalindrome("RADAR")); // true
+// console.log(checkPalindrome("mopar")); // false
+// console.log(checkPalindrome("A MAN, a plan, a CANAL, pAnAmA")); // true
+// console.log(checkPalindrome("RACEcar")); //true
+// console.log(checkPalindrome("Anna")); // true
+// console.log(checkPalindrome("GrandMa")); // false
+// console.log(checkPalindrome("catNip")); // false
 
 //=========================================
 // Sum Array
 //=========================================
 
-let sum = 0;
+// let sum = 0;
 
-const sumfunc = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  console.log(sum);
-  return sum;
-};
+// const sumfunc = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   console.log(sum);
+//   return sum;
+// };
 
-sumfunc([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]); // 55
-sumfunc([2, 4, 6, 8, -40, 32]); // 67
+// sumfunc([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]); // 55
+// sumfunc([2, 4, 6, 8, -40, 32]); // 67
 
 //=========================================
 // Prime Numbers
 //=========================================
 
-const isItPrime = (num) => {
-  if (num <= 1) return false;
-  for (let i = 2; i < Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return `${num} is not a prime number`;
-    }
-  }
-  return `${num} is a prime number`;
-};
+// const isItPrime = (num) => {
+//   if (num <= 1) return false;
+//   for (let i = 2; i < Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return `${num} is not a prime number`;
+//     }
+//   }
+//   return `${num} is a prime number`;
+// };
 
 // console.log(isItPrime(95)); //false
 // console.log(isItPrime(64)); //false
@@ -72,10 +72,21 @@ const isItPrime = (num) => {
 // console.log(isItPrime(10647)); //false
 // console.log(isItPrime(7)); //true
 
-const printPrime = (num) => {
-  for (let i = 2; i < num; i++) {
-    console.log(isItPrime(i));
-  }
-};
+// const printPrime = (num) => {
+//   for (let i = 2; i < num; i++) {
+//     console.log(isItPrime(i));
+//   }
+// };
 
-printPrime(97); // should log everything that is and is not prime up to 97
+// printPrime(97); // should log everything that is and is not prime up to 97
+
+//=========================================
+// Rock Paper Scissors
+//=========================================
+
+const randomMove = () => {
+  const choices = ["rock", "paper", "scissors"];
+  let moves = Math.floor(Math.random() * choices.length);
+  return moves;
+};
+console.log(randomMove());
